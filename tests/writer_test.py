9ffd1c9badb8c5c6e7976fe19e7df7f5f33a867c
mostaphaRoudsari <sadeghipour@gmail.com>
2025-11-03 15:10:33 -0500

    feat(writer): add support for assigning zone as group property in IDM

--- tests/writer_test.py
+++ tests/writer_test.py
@@ -22,3 +22,17 @@ def test_non_ascii():
     bldg_file = out_folder.joinpath('single-room', 'single-room.idm')
     content = bldg_file.read_text(encoding='UTF-8')
     assert 'CE-ZONE :N "Wände" :T ZONE' in content
+
+
+def test_group_attribute():
+    in_file = './tests/assets/rooms-with-zone-properties.hbjson'
+    out_folder = pathlib.Path('./tests/assets/temp')
+    out_folder.mkdir(parents=True, exist_ok=True)
+    model = Model.from_hbjson(in_file)
+    outf = model_to_idm(model, out_folder.as_posix(), name='rooms-with-zone-properties', debug=True)
+    assert outf.is_file()
+    bldg_file = out_folder.joinpath(
+        'rooms-with-zone-properties', 'rooms-with-zone-properties', '102.idm'
+    )
+    content = bldg_file.read_text(encoding='UTF-8')
+    assert '(:PAR :N GROUP :V "Lobby")' in content
